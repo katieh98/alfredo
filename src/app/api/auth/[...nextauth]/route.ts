1b4ac9849c3eb2de4fc55eeae1ebdd987c916c57
Katie Harold <ktharold7@gmail.com>
2026-04-24 14:53:58 -0700

    debug: enable NextAuth debug logging

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,7 @@ import NextAuth, { type AuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
 export const authOptions: AuthOptions = {
+  debug: true,
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID!,
